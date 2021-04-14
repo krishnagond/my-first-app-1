931e4a99f87f6d423137c99c928e568527875dd9
krishnagond <krgond786mani@gmail.com>
2021-04-14 16:05:51 +0530

    Revert "Revert "Our team""
    This reverts commit d9709d9025514aff291f525234e24b9744056de6.

--- my-first-project/src/app/app.module.ts
+++ my-first-project/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -9,6 +8,10 @@ import { FirstComponent } from './first/first.component';
 import { SecondComponent } from './second/second.component';
 import { SignupComponent } from './signup/signup.component';
 import { ProfilepageComponent } from './profilepage/profilepage.component';
+import { UploadfileComponent } from './uploadfile/uploadfile.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthinterceptorServiceProvider } from './auth/authinterceptor.service';
+
 
 @NgModule({
   declarations: [
@@ -17,14 +20,18 @@ import { ProfilepageComponent } from './profilepage/profilepage.component';
     FirstComponent,
     SecondComponent,
     SignupComponent,
-    ProfilepageComponent
+    ProfilepageComponent,
+    UploadfileComponent
   ],
+
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  
+  providers: [AuthinterceptorServiceProvider],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
